2951c759941e18802b9014ec355306c28b021c7f
God <netherchef@gmail.com>
2019-02-05 04:02:17 +0800

    feat [Yarn Adventure]
    Also added [Yarn name prefix detection].

---
+++ PlayPlayProject/Assets/NPCDialogueScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class NPCDialogueScript : MonoBehaviour {
+
+	// Scripts
+
+	DialogueRunner dialogueRunner;
+	public DialogueRunner player_DR;
+
+	// NPC Dialogue Variables
+
+	bool canTalk;
+
+	private void Start () {
+
+		// Scripts
+
+		dialogueRunner = GetComponent<DialogueRunner> ();
+	}
+
+	private void Update () {
+		if (Input.GetButtonDown ("Interact") && canTalk) StartConversation ("Tuesday");
+	}
+
+	#region On Triggers ________________________________________________________
+
+	private void OnTriggerEnter2D (Collider2D collision) {
+		if (collision.tag == "Player") canTalk = true;
+	}
+
+	private void OnTriggerExit2D (Collider2D collision) {
+		if (collision.tag == "Player") canTalk = false;
+	}
+
+	#endregion
+
+	void Talk (string node) {
+		dialogueRunner.StartDialogue (node);
+	}
+
+	void StartConversation (string node) {
+		dialogueRunner.StartDialogue (node);
+		player_DR.StartDialogue (node);
+	}
+}
---
+++ PlayPlayProject/Assets/PlayerMoveScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+	// Components
+
+	Transform _transform;
+
+	// Player Movement Variables
+
+	float movementSpeed = 10f;
+
+	// Enumerators
+
+	IEnumerator playerMove;
+
+	private void Start () {
+
+		// Components
+
+		_transform = transform;
+
+		#region Start Operations ...............................................
+
+		playerMove = PlayerMove ();
+		StartCoroutine (playerMove);
+
+		#endregion
+	}
+
+	IEnumerator PlayerMove () {
+
+		while (enabled) {
+
+			if (Input.GetAxisRaw ("Horizontal") != 0) {
+				_transform.Translate (new Vector3 (Mathf.Sign (Input.GetAxisRaw ("Horizontal") * movementSpeed * Time.deltaTime), 0, 0));
+			}
+
+			yield return null;
+		}
+	}
+}
